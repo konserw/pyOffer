77d439668092517c097a1819798b0b5070c2f72d
Kamil Strzempowicz <konserw@gmail.com>
2020-02-14 19:41:56 +0100

    Use pytest instead of unittest

--- tests/customer.py
+++ tests/customer.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 from hamcrest import assert_that, is_
 from src.customer import CustomerFactory, Customer
 
@@ -17,40 +17,35 @@ class MockDB:
         return (CUSTOMER_ID, SHORT_NAME, FULL_NAME, TITLE, FIRST_NAME, LAST_NAME, ADDRESS)
 
 
-class TestCustomer(unittest.TestCase):
-    def setUp(self) -> None:
-        self.factory = CustomerFactory(MockDB())
+@pytest.fixture
+def customer():
+    factory = CustomerFactory(MockDB())
+    return factory.get_customer_from_id(CUSTOMER_ID)
 
-    def test_get_customer(self):
-        c = self.factory.get_customer_from_id(CUSTOMER_ID)
-        assert_that(c.is_valid, is_(True))
-        assert_that(c.id, is_(CUSTOMER_ID))
-        assert_that(c.short_name, is_(SHORT_NAME))
-        assert_that(c.full_name, is_(FULL_NAME))
-        assert_that(c.title, is_(TITLE))
-        assert_that(c.last_name, is_(LAST_NAME))
-        assert_that(c.address, is_(ADDRESS))
 
-    def test_concated_name(self):
-        c = self.factory.get_customer_from_id(CUSTOMER_ID)
-        assert_that(c.concated_name, is_("Mr. John Doe"))
+class TestCustomer:
+    def test_get_customer(self, customer):
+        assert_that(customer.is_valid, is_(True))
+        assert_that(customer.id, is_(CUSTOMER_ID))
+        assert_that(customer.short_name, is_(SHORT_NAME))
+        assert_that(customer.full_name, is_(FULL_NAME))
+        assert_that(customer.title, is_(TITLE))
+        assert_that(customer.last_name, is_(LAST_NAME))
+        assert_that(customer.address, is_(ADDRESS))
+
+    def test_concated_name(self, customer):
+        assert_that(customer.concated_name, is_("Mr. John Doe"))
 
     def test_is_not_valid(self):
-        c = Customer()
-        assert_that(c.is_valid, is_(False))
+        customer = Customer()
+        assert_that(customer.is_valid, is_(False))
 
-    def test_html_address(self):
-        c = self.factory.get_customer_from_id(CUSTOMER_ID)
-        assert_that(c.html_address, is_("255 Some street<br />\nIn some town"))
+    def test_html_address(self, customer):
+        assert_that(customer.html_address, is_("255 Some street<br />\nIn some town"))
 
-    def test_db_id(self):
-        c = self.factory.get_customer_from_id(CUSTOMER_ID)
-        assert_that(c.db_id, is_("1"))
+    def test_db_id(self, customer):
+        assert_that(customer.db_id, is_("1"))
 
     def test_null_id(self):
-        c = Customer()
-        assert_that(c.db_id, is_("NULL"))
-
-
-if __name__ == '__main__':
-    unittest.main()
+        customer = Customer()
+        assert_that(customer.db_id, is_("NULL"))
--- tests/merchandise.py
+++ tests/merchandise.py
@@ -1,10 +1,7 @@
-import unittest
+import pytest
+from hamcrest import assert_that, is_
 
 
-class MyTestCase(unittest.TestCase):
+class TestMerchandise:
     def test_something(self):
-        self.assertEqual(True, False)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert_that(True, is_(False))
