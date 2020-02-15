094fb6712b76cb01701e5e6adc3616e922e77cc2
Kamil Strzempowicz <konserw@gmail.com>
2020-02-15 15:08:55 +0100

    Make MainWindow appear by adding MerchandiseListView

--- src/merchandise.py
+++ src/merchandise.py
@@ -1,8 +1,16 @@
-class Merchandise(object):
+from PyQt5.QtWidgets import QWidget
+
+
+class Merchandise:
     def __init__(self):
         pass
 
 
-class MerchandiseList(object):
+class MerchandiseList:
     def __init__(self):
         pass
+
+
+class MerchandiseListView(QWidget):
+    def __init__(self, parent):
+        super().__init__(parent)
