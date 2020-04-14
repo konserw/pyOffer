897725579e5a21ad076579b8c6c1dba0a9c6fdf5
Kamil Strzempowicz <konserw@gmail.com>
2020-04-15 00:56:13 +0200

    Move complex inquiry_number_toggled test to e2e tests and expand it

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -9,16 +9,25 @@
 #  If not, see <http://www.gnu.org/licenses/>.
 
 import pytest
-from hamcrest import assert_that, is_, contains_exactly, empty
+from PySide2.QtCore import Qt, QPoint
+from hamcrest import assert_that, is_, none, contains_exactly, empty
+from qtmatchers import disabled, enabled
+
 from src.database import get_user_record
 from src.main_window import MainWindow
 from src.user import User
 from tests.test_merchandise import create_merch
-from PySide2.QtCore import Qt, QPoint
+
+
+@pytest.fixture
+def active_window(db):
+    user = User.from_sql_record(get_user_record(1))
+    window = MainWindow(user)
+    window.new_offer()
+    return window
 
 
 @pytest.mark.e2e
-@pytest.mark.usefixtures("db")
 class TestEndToEnd:
     @pytest.mark.parametrize("rows, expected", [
         pytest.param([0], "second"),
@@ -27,20 +36,17 @@ class TestEndToEnd:
         pytest.param([1, 0], "none"),
         pytest.param([0, 0], "none"),
     ])
-    def test_remove_row(self, qtbot, rows, expected):
-        user = User.from_sql_record(get_user_record(1))
-        main_window = MainWindow(user)
-        main_window.new_offer()
+    def test_remove_row(self, qtbot, active_window, rows, expected):
         m1 = create_merch(1)
         m2 = create_merch(2)
-        main_window.offer.merchandise_list.change_item_count(m1)
-        main_window.offer.merchandise_list.change_item_count(m2)
+        active_window.offer.merchandise_list.change_item_count(m1)
+        active_window.offer.merchandise_list.change_item_count(m2)
 
         for row in rows:
-            x = main_window.ui.tableView.columnViewportPosition(1) + 5
-            y = main_window.ui.tableView.rowViewportPosition(row) + 5
-            qtbot.mouseClick(main_window.ui.tableView.viewport(), Qt.LeftButton, Qt.NoModifier, QPoint(x, y))
-            qtbot.mouseClick(main_window.ui.push_button_remove_row, Qt.LeftButton)
+            x = active_window.ui.tableView.columnViewportPosition(1) + 5
+            y = active_window.ui.tableView.rowViewportPosition(row) + 5
+            qtbot.mouseClick(active_window.ui.tableView.viewport(), Qt.LeftButton, Qt.NoModifier, QPoint(x, y))
+            qtbot.mouseClick(active_window.ui.push_button_remove_row, Qt.LeftButton)
 
         merch_list = {
             "none": [],
@@ -50,12 +56,12 @@ class TestEndToEnd:
         }
         expected_merch_list = merch_list[expected]
         if expected_merch_list:
-            assert_that(main_window.offer.merchandise_list.list, contains_exactly(*expected_merch_list))
+            assert_that(active_window.offer.merchandise_list.list, contains_exactly(*expected_merch_list))
         else:
-            assert_that(main_window.offer.merchandise_list.list, is_(empty()))
+            assert_that(active_window.offer.merchandise_list.list, is_(empty()))
 
         expected_item_count = len(expected_merch_list)
-        assert_that(main_window.ui.tableView.model().rowCount(), is_(expected_item_count + 1))
+        assert_that(active_window.ui.tableView.model().rowCount(), is_(expected_item_count + 1))
 
         total = {
             "none": "0.00",
@@ -64,9 +70,24 @@ class TestEndToEnd:
             "both": "17.98",
         }
         expected_grand_total = total[expected]
-        x = main_window.ui.tableView.columnViewportPosition(7) + 5
-        y = main_window.ui.tableView.rowViewportPosition(expected_item_count) + 5
-        grand_total = main_window.ui.tableView.indexAt(QPoint(x, y)).data(Qt.DisplayRole)
+        x = active_window.ui.tableView.columnViewportPosition(7) + 5
+        y = active_window.ui.tableView.rowViewportPosition(expected_item_count) + 5
+        grand_total = active_window.ui.tableView.indexAt(QPoint(x, y)).data(Qt.DisplayRole)
         assert_that(grand_total, is_(expected_grand_total))
 
+    def test_inquiry_number_toggled(self, qtbot, active_window):
+        expected_query_number = "Lorem ipsum"
+        assert_that(active_window.offer.inquiry_number, is_(none()))
+        assert_that(active_window.ui.line_edit_query_number, is_(disabled()))
 
+        active_window.inquiry_number_toggled(Qt.Checked)  # enable
+        assert_that(active_window.ui.line_edit_query_number, is_(enabled()))
+
+        # fill with anything
+        qtbot.keyClicks(active_window.ui.line_edit_query_number, expected_query_number)
+        assert_that(active_window.offer.inquiry_number, is_(expected_query_number))
+
+        active_window.inquiry_number_toggled(Qt.Unchecked)  # disable
+        assert_that(active_window.ui.line_edit_query_number, is_(disabled()))
+        assert_that(active_window.ui.line_edit_query_number.text(), is_(""))
+        assert_that(active_window.offer.inquiry_number, is_(empty()))
