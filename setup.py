27806138460a3f28d1c0803998f52bc228d23a1e
Kamil Strzempowicz <konserw@gmail.com>
2020-04-21 16:10:15 +0200

    Improve setup.py so that installation file is smaller

--- setup.py
+++ setup.py
@@ -1,18 +1,30 @@
+import os
+import sys
+
+import PySide2
 from cx_Freeze import setup, Executable
 
-# Dependencies are automatically detected, but it might need
-# fine tuning.
-buildOptions = dict(include_files=["libssl-1_1-x64.dll", "libpq.dll", "libintl-8.dll", "libiconv-2.dll", "libcrypto-1_1-x64.dll"])
+plugins_path = os.path.join(PySide2.__path__[0], "plugins")
 
-import sys
-base = 'Win32GUI' if sys.platform=='win32' else None
+buildOptions = {
+    "include_files": ["libssl-1_1-x64.dll", "libpq.dll", "libintl-8.dll", "libiconv-2.dll", "libcrypto-1_1-x64.dll",
+                      os.path.join(plugins_path, "sqldrivers"),
+                      os.path.join(plugins_path, "platforms"),
+                      os.path.join(plugins_path, "printsupport"),
+                      ],
+    "excludes": [],
+    "zip_include_packages": "PySide2",
+    "optimize": 2
+}
+
+base = 'Win32GUI' if sys.platform == 'win32' else None
 
 executables = [
-    Executable('main.py', base=base, targetName = 'pyOffer')
+    Executable('main.py', base=base, targetName='pyOffer')
 ]
 
 setup(name='pyOffer',
-      version = '0.1',
-      description = 'Program for creating business proposals for purchase of items',
-      options = dict(build_exe = buildOptions),
-      executables = executables)
+      version='0.1',
+      description='Program for creating business proposals for purchase of items',
+      options=dict(build_exe=buildOptions),
+      executables=executables)
