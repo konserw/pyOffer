883e3990a0c0bca9effb84990be74b1ec54e3389
Kamil Strzempowicz <konserw@gmail.com>
2020-07-30 23:43:19 +0200

    Bump version after release

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from src import database
 from src.main_window import MainWindow
 from src.user import User, UserSelectionDialog
 
-VERSION = 0.9
+VERSION = 1.0
 
 if __name__ == '__main__':
     if getattr(sys, 'frozen', False):
